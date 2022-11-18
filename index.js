c52923068744b1e49799e43f7b96f279f7a287cb
S KUMAR <95487014+S-kumar856@users.noreply.github.com>
2022-11-18 22:39:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const http = require("http");
+const fs = require("fs");
+
+fs.readFile("home.html", (err, home) => {
+    console.log(home.toString());
+  });
+
+const port = require('minimist')(process.argv.slice(2), {
+    default: { greeting: 'Hello' }
+  })
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  })
+
+http
+  .createServer((request, response) => {
+    const url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case '/registration':
+        response.write(registrationContent)
+        response.end()
+        break
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
