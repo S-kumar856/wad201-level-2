e22e8d7cf6d885e0ffd9b2b354c08e1f3091d028
S KUMAR <95487014+S-kumar856@users.noreply.github.com>
2022-11-19 12:27:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,51 @@
-const http = require("http");
-const fs = require("fs");
-
-fs.readFile("home.html", (err, home) => {
-    console.log(home.toString());
-  });
+const http = require('http')
+const fs = require('fs')
 
 const port = require('minimist')(process.argv.slice(2), {
-    default: { greeting: 'Hello' }
-  })
+  default: { greeting: 'Hello' }
+})
+let homeContent = ''
+let projectContent = ''
+let registrationContent = ''
 
-let homeContent = "";
-let projectContent = "";
-let registrationContent = "";
-
-fs.readFile("home.html", (err, home) => {
+fs.readFile('home.html', (err, home) => {
   if (err) {
-    throw err;
+    throw err
   }
-  homeContent = home;
-});
+  homeContent = home
+})
 
-fs.readFile("project.html", (err, project) => {
+fs.readFile('project.html', (err, project) => {
   if (err) {
-    throw err;
+    throw err
   }
-  projectContent = project;
-});
+  projectContent = project
+})
 
-fs.readFile("registration.html", (err, registration) => {
-    if (err) {
-      throw err;
-    }
-    registrationContent = registration;
-  })
+fs.readFile('registration.html', (err, registration) => {
+  if (err) {
+    throw err
+  }
+  registrationContent = registration
+})
 
 http
   .createServer((request, response) => {
-    const url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
+    const url = request.url
+    response.writeHeader(200, { 'Content-Type': 'text/html' })
     switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
+      case '/project':
+        response.write(projectContent)
+        response.end()
+        break
       case '/registration':
         response.write(registrationContent)
         response.end()
         break
       default:
-        response.write(homeContent);
-        response.end();
-        break;
+        response.write(homeContent)
+        response.end()
+        break
     }
   })
-  .listen(3000);
+  .listen(port)
